bc34f95e5456db46e87acb05efaee287962c2618
Farouk Farag <ffarag@proemcs.com>
2021-05-18 10:55:44 +0200

    Change project local directory.

--- Freelance/Properties/AssemblyInfo.cs
+++ Freelance/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ca53250f-f9ea-40f5-a73e-965c48941e8c")]
+[assembly: Guid("723041bf-cf2b-4967-a5f0-65cc9eeff95b")]
 
 // Version information for an assembly consists of the following four values:
 //
